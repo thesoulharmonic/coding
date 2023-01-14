de7d799ec5bea0c928f3e4b2fc930411c9b5b972
thesoulharmonic <thesoulharmonic@gmail.com>
2023-01-14 23:10:23 +0000

    uploaded testingBranch.js

--- testingBranch.js
+++ testingBranch.js
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 //test branch/Users/richwilliams/Desktop/Coding/testingBranch.js
 
 const test = []
+=======
+//test branch
+>>>>>>> 8f2c549 (uploaded testingBranch.js)
 =======
 //test branch
 >>>>>>> 8f2c549 (uploaded testingBranch.js)
