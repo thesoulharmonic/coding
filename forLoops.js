8767ba033bd744ffb4af06c069b2b94f84565789
thesoulharmonic <thesoulharmonic@gmail.com>
2023-01-15 12:00:49 +0000

    un commented 15/1/23 RW

--- forLoops.js
+++ forLoops.js
@@ -1,33 +1,33 @@
-// // Setup
-// const myArray = [];
+// Setup
+const myArray = [];
 
-// // Only change code below this line
+// Only change code below this line
 
-// for (let i = 9; i >= 0; i--) {
-//   if (i % 2 != 0){myArray.push(i);
-//   }
-// }
+for (let i = 9; i >= 0; i--) {
+  if (i % 2 != 0){myArray.push(i);
+  }
+}
 
-// console.log(myArray)
+console.log(myArray)
 
-// // Setup
-// const myArr = [2, 3, 4, 5, 6];
-// // Only change code below this line
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+// Only change code below this line
 
 
-// let total = myArr.reduce((acc, cur) => acc + cur, 0) // as a single return reduce function5
-//    console.log(total)
+let total = myArr.reduce((acc, cur) => acc + cur, 0) // as a single return reduce function5
+   console.log(total)
 
-//// Another way to do it
+// Another way to do it
 
-// // Setup
-// const myArr = [2, 3, 4, 5, 6];
-// // Only change code below this line
-// let total = 0;
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+// Only change code below this line
+let total = 0;
 
-// for (let i = 0; i < myArr.length; i++) {
-//   total += myArr[i];
-//    console.log(myArr[i]);}
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+   console.log(myArr[i]);}
  
 
 function multiplyAll(arr) {
@@ -49,3 +49,5 @@ function multiplyAll(arr) {
   }
   
   multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+  // git test 
